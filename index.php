dadf52a5dfea36dbc141dc7e5e8ac5059847a1fc
LucaPierantoni <luca.pier99@gmail.com>
2024-06-20 17:07:25 +0200

    UPD Exercise Complete (No Bonus)

--- index.php
+++ index.php
@@ -3,23 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DischiJson</title>
+    <title>DiscJson</title>
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- vue 3 -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <!-- axios -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
 <div id="app">
-    <h1>DischiJson</h1>
-    <ul class="mb-4">
-        <li v-for="currentDisc in discList">
-            {{currentDisc.title}}
-        </li>
-    </ul>
+    <h1>DiscJson</h1>
+    <section>
+        <div v-for="currentDisc in discList" class="card mb-4 text-center my_card" style="width: 18rem;">
+            <img :src="currentDisc.poster" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{currentDisc.title}}</h5>
+                <p class="card-text">{{currentDisc.author}}</p>
+                <h6 class="card-title">{{currentDisc.year}}</h6>
+            </div>
+        </div>
+    </section>
 </div> 
 
     <script src="./js/main.js"></script>
